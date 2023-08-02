ae42a6161133aa7a5bc91fcd2634daceb778114d
Rushendra Sai Vutukuri <rushivutukuri@gmail.com>
2023-08-02 19:32:17 +0530

    LinearSearch by recursion

--- src/Algorithms/Searching/LinearSearch.java
+++ src/Algorithms/Searching/LinearSearch.java
@@ -3,8 +3,8 @@ package Algorithms.Searching;
 public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {9,2,5,0,8,4,7,3,6,1};
-        int number = 7;
-        if (linearSearch(arr, number) == -1) {
+        int number = 10;
+        if (linearSearchByRecursion(arr, number) == -1) {
             System.out.println("The number is not in the array");
         }
         else {
@@ -19,5 +19,23 @@ public class LinearSearch {
         }
         return -1;
     }
+
+    public static int linearSearchByRecursion(int[] arr, int n) {
+        return linearSearch(0, arr,n);
+    }
+
+
+    private static int linearSearch(int index, int[] arr, int n) {
+        if (index == arr.length) {
+            return -1;
+        }
+        if (arr[index] == n) {
+            return index;
+        }
+        else {
+            return linearSearch(index+1,arr,n);
+        }
+    }
+
 
 }
