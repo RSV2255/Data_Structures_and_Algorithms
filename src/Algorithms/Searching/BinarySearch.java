eee242d42ed1ec5a8e87e6cd18deb02a557f31e8
RSV2255 <rushivutukuri@gmail.com>
2023-08-04 10:14:52 +0530

    Binary search in 2D arrays

--- src/Algorithms/Searching/BinarySearch.java
+++ src/Algorithms/Searching/BinarySearch.java
@@ -1,11 +1,26 @@
 package Algorithms.Searching;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     public static void main(String[] args) {
+
         int[] arr = {2,3,5,7,9,11,13,14,16};
 
-        int index = binarySearchByRecursion(arr, 14);
+        int index = binarySearch(arr, 11);
         System.out.println(index);
+
+        int index2 = binarySearchByRecursion(arr, 14);
+        System.out.println(index2);
+
+        int[][] arr2 = {
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+        int[] ints = binarySearch(arr2, 33);
+        System.out.println(Arrays.toString(ints));
 
 
     }
@@ -42,7 +57,23 @@ public class BinarySearch {
             return binarySearch(start, mid-1,arr2,key);
         else
             return mid;
+    }
 
+    public static int[] binarySearch(int[][] arr, int target) {
+        int r = 0;
+        int c = arr.length-1;
+
+        while (r < arr.length && c >= 0) {
+            if (arr[r][c] == target) {
+                return new int[] {r,c};
+            }
+            if (target < arr[r][c])
+                c--;
+            else {
+                r++;
+            }
+        }
+        return new int[] {-1,-1};
     }
 
 }
